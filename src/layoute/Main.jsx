fec6079e6b41cd2197dca41517ca37bd9e1f024c
Anton101-prog <60011626+Anton101-prog@users.noreply.github.com>
2021-02-28 15:59:52 +0300

    nothing

--- src/layoute/Main.jsx
+++ src/layoute/Main.jsx
@@ -12,7 +12,7 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }))
       .catch((err) => {
