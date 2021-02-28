809c179e3b1ee8073f794a7dcc296cf22a6f53ca
Anton101-prog <60011626+Anton101-prog@users.noreply.github.com>
2021-02-28 15:43:02 +0300

    changes

--- src/layoute/Main.jsx
+++ src/layoute/Main.jsx
@@ -12,22 +12,28 @@ class Main extends React.Component {
   };
 
   componentDidMount() {
-    fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`
-    )
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   }
 
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `http://www.omdbapi.com/?apikey=1a50583&s=${str}${
+      `https://www.omdbapi.com/?apikey=1a50583&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .catch((err) => {
+        console.error(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
