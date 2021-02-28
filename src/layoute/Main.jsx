73aa7f2c8d3729be3b5194acaca518dc49912c54
Anton101-prog <60011626+Anton101-prog@users.noreply.github.com>
2021-02-28 16:04:16 +0300

    already did

--- src/layoute/Main.jsx
+++ src/layoute/Main.jsx
@@ -24,7 +24,7 @@ class Main extends React.Component {
   searchMovies = (str, type = "all") => {
     this.setState({ loading: true });
     fetch(
-      `https://www.omdbapi.com/?apikey=1a50583&s=${str}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
         type !== "all" ? `&type=${type}` : ""
       }`
     )
